4a9cac3b06496eee78b8775270fce05d65885e21
Chris Schrijver <chris@raow.nl>
2018-12-28 00:03:33 +0100

    minor fixes

--- app/Services/SiteService.php
+++ app/Services/SiteService.php
@@ -7,8 +7,8 @@ class SiteService extends AbstractService
 {
     public function register()
     {
-        offbeat('post-type')->registerPostModel('post', \OffbeatWP\Models\PostModel::class);
-        offbeat('post-type')->registerPostModel('page', \OffbeatWP\Models\PageModel::class);
+        offbeat('post-type')->registerPostModel('post', \App\Models\PostModel::class);
+        offbeat('post-type')->registerPostModel('page', \App\Models\PageModel::class);
 
         add_theme_support('custom-header');
         add_theme_support('post-thumbnails');
